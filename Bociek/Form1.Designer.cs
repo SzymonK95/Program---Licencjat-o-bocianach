d52040317354e3b447448b3c3198513c773e05a3
Szymon <szymon_kaszuba97@wp.pl>
2017-04-09 22:57:40 +0200

    Działa zgodnie z oczekiwaniami, nie zawsze, nie idiotoodporny

---
+++ Bociek/Form1.Designer.cs
@@ -0,0 +1,376 @@
+﻿namespace Bociek
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel9 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel10 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel5 = new System.Windows.Forms.TableLayoutPanel();
+            this.textBoxExtraInfo = new System.Windows.Forms.TextBox();
+            this.labelExtraInfo = new System.Windows.Forms.Label();
+            this.tableLayoutPanel8 = new System.Windows.Forms.TableLayoutPanel();
+            this.buttonRead = new System.Windows.Forms.Button();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.tableLayoutPanel11 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel12 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel4 = new System.Windows.Forms.TableLayoutPanel();
+            this.comboBoxWhatDo = new System.Windows.Forms.ComboBox();
+            this.labelWhatDo = new System.Windows.Forms.Label();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.comboBoxWho = new System.Windows.Forms.ComboBox();
+            this.labelWho = new System.Windows.Forms.Label();
+            this.tableLayoutPanel13 = new System.Windows.Forms.TableLayoutPanel();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.textBoxHowLong = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel6 = new System.Windows.Forms.TableLayoutPanel();
+            this.textBoxStart = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel7 = new System.Windows.Forms.TableLayoutPanel();
+            this.textBoxStop = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonDate = new System.Windows.Forms.Button();
+            this.buttonClear2 = new System.Windows.Forms.Button();
+            this.labelName = new System.Windows.Forms.Label();
+            this.listBoxLogs = new System.Windows.Forms.ListBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel9.SuspendLayout();
+            this.tableLayoutPanel10.SuspendLayout();
+            this.tableLayoutPanel5.SuspendLayout();
+            this.tableLayoutPanel8.SuspendLayout();
+            this.tableLayoutPanel11.SuspendLayout();
+            this.tableLayoutPanel12.SuspendLayout();
+            this.tableLayoutPanel4.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.tableLayoutPanel13.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel6.SuspendLayout();
+            this.tableLayoutPanel7.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            resources.ApplyResources(this.tableLayoutPanel1, "tableLayoutPanel1");
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel9, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.labelName, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.listBoxLogs, 0, 1);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            // 
+            // tableLayoutPanel9
+            // 
+            resources.ApplyResources(this.tableLayoutPanel9, "tableLayoutPanel9");
+            this.tableLayoutPanel9.Controls.Add(this.tableLayoutPanel10, 1, 0);
+            this.tableLayoutPanel9.Controls.Add(this.tableLayoutPanel11, 0, 0);
+            this.tableLayoutPanel9.Name = "tableLayoutPanel9";
+            // 
+            // tableLayoutPanel10
+            // 
+            resources.ApplyResources(this.tableLayoutPanel10, "tableLayoutPanel10");
+            this.tableLayoutPanel10.Controls.Add(this.tableLayoutPanel5, 0, 0);
+            this.tableLayoutPanel10.Controls.Add(this.tableLayoutPanel8, 0, 1);
+            this.tableLayoutPanel10.Name = "tableLayoutPanel10";
+            // 
+            // tableLayoutPanel5
+            // 
+            resources.ApplyResources(this.tableLayoutPanel5, "tableLayoutPanel5");
+            this.tableLayoutPanel5.Controls.Add(this.textBoxExtraInfo, 0, 1);
+            this.tableLayoutPanel5.Controls.Add(this.labelExtraInfo, 0, 0);
+            this.tableLayoutPanel5.Name = "tableLayoutPanel5";
+            // 
+            // textBoxExtraInfo
+            // 
+            resources.ApplyResources(this.textBoxExtraInfo, "textBoxExtraInfo");
+            this.textBoxExtraInfo.Name = "textBoxExtraInfo";
+            // 
+            // labelExtraInfo
+            // 
+            resources.ApplyResources(this.labelExtraInfo, "labelExtraInfo");
+            this.labelExtraInfo.Name = "labelExtraInfo";
+            // 
+            // tableLayoutPanel8
+            // 
+            resources.ApplyResources(this.tableLayoutPanel8, "tableLayoutPanel8");
+            this.tableLayoutPanel8.Controls.Add(this.buttonRead, 1, 0);
+            this.tableLayoutPanel8.Controls.Add(this.buttonAdd, 0, 0);
+            this.tableLayoutPanel8.Name = "tableLayoutPanel8";
+            // 
+            // buttonRead
+            // 
+            resources.ApplyResources(this.buttonRead, "buttonRead");
+            this.buttonRead.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.buttonRead.Name = "buttonRead";
+            this.buttonRead.UseVisualStyleBackColor = false;
+            this.buttonRead.Click += new System.EventHandler(this.buttonRead_Click);
+            // 
+            // buttonAdd
+            // 
+            resources.ApplyResources(this.buttonAdd, "buttonAdd");
+            this.buttonAdd.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.UseVisualStyleBackColor = false;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // tableLayoutPanel11
+            // 
+            resources.ApplyResources(this.tableLayoutPanel11, "tableLayoutPanel11");
+            this.tableLayoutPanel11.Controls.Add(this.tableLayoutPanel12, 0, 0);
+            this.tableLayoutPanel11.Controls.Add(this.tableLayoutPanel13, 0, 1);
+            this.tableLayoutPanel11.Name = "tableLayoutPanel11";
+            // 
+            // tableLayoutPanel12
+            // 
+            resources.ApplyResources(this.tableLayoutPanel12, "tableLayoutPanel12");
+            this.tableLayoutPanel12.Controls.Add(this.tableLayoutPanel4, 0, 0);
+            this.tableLayoutPanel12.Controls.Add(this.tableLayoutPanel3, 0, 0);
+            this.tableLayoutPanel12.Name = "tableLayoutPanel12";
+            // 
+            // tableLayoutPanel4
+            // 
+            resources.ApplyResources(this.tableLayoutPanel4, "tableLayoutPanel4");
+            this.tableLayoutPanel4.Controls.Add(this.comboBoxWhatDo, 0, 1);
+            this.tableLayoutPanel4.Controls.Add(this.labelWhatDo, 0, 0);
+            this.tableLayoutPanel4.Name = "tableLayoutPanel4";
+            // 
+            // comboBoxWhatDo
+            // 
+            resources.ApplyResources(this.comboBoxWhatDo, "comboBoxWhatDo");
+            this.comboBoxWhatDo.FormattingEnabled = true;
+            this.comboBoxWhatDo.Items.AddRange(new object[] {
+            resources.GetString("comboBoxWhatDo.Items"),
+            resources.GetString("comboBoxWhatDo.Items1"),
+            resources.GetString("comboBoxWhatDo.Items2"),
+            resources.GetString("comboBoxWhatDo.Items3"),
+            resources.GetString("comboBoxWhatDo.Items4"),
+            resources.GetString("comboBoxWhatDo.Items5"),
+            resources.GetString("comboBoxWhatDo.Items6"),
+            resources.GetString("comboBoxWhatDo.Items7"),
+            resources.GetString("comboBoxWhatDo.Items8"),
+            resources.GetString("comboBoxWhatDo.Items9"),
+            resources.GetString("comboBoxWhatDo.Items10"),
+            resources.GetString("comboBoxWhatDo.Items11"),
+            resources.GetString("comboBoxWhatDo.Items12")});
+            this.comboBoxWhatDo.Name = "comboBoxWhatDo";
+            // 
+            // labelWhatDo
+            // 
+            resources.ApplyResources(this.labelWhatDo, "labelWhatDo");
+            this.labelWhatDo.Name = "labelWhatDo";
+            // 
+            // tableLayoutPanel3
+            // 
+            resources.ApplyResources(this.tableLayoutPanel3, "tableLayoutPanel3");
+            this.tableLayoutPanel3.Controls.Add(this.comboBoxWho, 0, 1);
+            this.tableLayoutPanel3.Controls.Add(this.labelWho, 0, 0);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            // 
+            // comboBoxWho
+            // 
+            resources.ApplyResources(this.comboBoxWho, "comboBoxWho");
+            this.comboBoxWho.FormattingEnabled = true;
+            this.comboBoxWho.Items.AddRange(new object[] {
+            resources.GetString("comboBoxWho.Items"),
+            resources.GetString("comboBoxWho.Items1"),
+            resources.GetString("comboBoxWho.Items2"),
+            resources.GetString("comboBoxWho.Items3"),
+            resources.GetString("comboBoxWho.Items4")});
+            this.comboBoxWho.Name = "comboBoxWho";
+            // 
+            // labelWho
+            // 
+            resources.ApplyResources(this.labelWho, "labelWho");
+            this.labelWho.Name = "labelWho";
+            // 
+            // tableLayoutPanel13
+            // 
+            resources.ApplyResources(this.tableLayoutPanel13, "tableLayoutPanel13");
+            this.tableLayoutPanel13.Controls.Add(this.buttonClear, 3, 0);
+            this.tableLayoutPanel13.Controls.Add(this.tableLayoutPanel2, 0, 1);
+            this.tableLayoutPanel13.Controls.Add(this.tableLayoutPanel6, 0, 0);
+            this.tableLayoutPanel13.Controls.Add(this.tableLayoutPanel7, 2, 0);
+            this.tableLayoutPanel13.Controls.Add(this.buttonDate, 1, 0);
+            this.tableLayoutPanel13.Controls.Add(this.buttonClear2, 1, 1);
+            this.tableLayoutPanel13.Name = "tableLayoutPanel13";
+            // 
+            // buttonClear
+            // 
+            resources.ApplyResources(this.buttonClear, "buttonClear");
+            this.buttonClear.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.UseVisualStyleBackColor = false;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // tableLayoutPanel2
+            // 
+            resources.ApplyResources(this.tableLayoutPanel2, "tableLayoutPanel2");
+            this.tableLayoutPanel2.Controls.Add(this.textBoxHowLong, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.label3, 0, 0);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            // 
+            // textBoxHowLong
+            // 
+            resources.ApplyResources(this.textBoxHowLong, "textBoxHowLong");
+            this.textBoxHowLong.Name = "textBoxHowLong";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // tableLayoutPanel6
+            // 
+            resources.ApplyResources(this.tableLayoutPanel6, "tableLayoutPanel6");
+            this.tableLayoutPanel6.Controls.Add(this.textBoxStart, 0, 1);
+            this.tableLayoutPanel6.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel6.Name = "tableLayoutPanel6";
+            // 
+            // textBoxStart
+            // 
+            resources.ApplyResources(this.textBoxStart, "textBoxStart");
+            this.textBoxStart.Name = "textBoxStart";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // tableLayoutPanel7
+            // 
+            resources.ApplyResources(this.tableLayoutPanel7, "tableLayoutPanel7");
+            this.tableLayoutPanel7.Controls.Add(this.textBoxStop, 0, 1);
+            this.tableLayoutPanel7.Controls.Add(this.label2, 0, 0);
+            this.tableLayoutPanel7.Name = "tableLayoutPanel7";
+            // 
+            // textBoxStop
+            // 
+            resources.ApplyResources(this.textBoxStop, "textBoxStop");
+            this.textBoxStop.Name = "textBoxStop";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // buttonDate
+            // 
+            resources.ApplyResources(this.buttonDate, "buttonDate");
+            this.buttonDate.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.buttonDate.Name = "buttonDate";
+            this.buttonDate.UseVisualStyleBackColor = false;
+            this.buttonDate.Click += new System.EventHandler(this.buttonDate_Click);
+            // 
+            // buttonClear2
+            // 
+            resources.ApplyResources(this.buttonClear2, "buttonClear2");
+            this.buttonClear2.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.buttonClear2.Name = "buttonClear2";
+            this.buttonClear2.UseVisualStyleBackColor = false;
+            this.buttonClear2.Click += new System.EventHandler(this.buttonClear2_Click);
+            // 
+            // labelName
+            // 
+            resources.ApplyResources(this.labelName, "labelName");
+            this.labelName.Name = "labelName";
+            // 
+            // listBoxLogs
+            // 
+            resources.ApplyResources(this.listBoxLogs, "listBoxLogs");
+            this.listBoxLogs.FormattingEnabled = true;
+            this.listBoxLogs.Name = "listBoxLogs";
+            this.listBoxLogs.Click += new System.EventHandler(this.listBoxLogs_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightBlue;
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel9.ResumeLayout(false);
+            this.tableLayoutPanel10.ResumeLayout(false);
+            this.tableLayoutPanel5.ResumeLayout(false);
+            this.tableLayoutPanel5.PerformLayout();
+            this.tableLayoutPanel8.ResumeLayout(false);
+            this.tableLayoutPanel11.ResumeLayout(false);
+            this.tableLayoutPanel12.ResumeLayout(false);
+            this.tableLayoutPanel4.ResumeLayout(false);
+            this.tableLayoutPanel4.PerformLayout();
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.tableLayoutPanel3.PerformLayout();
+            this.tableLayoutPanel13.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.tableLayoutPanel6.ResumeLayout(false);
+            this.tableLayoutPanel6.PerformLayout();
+            this.tableLayoutPanel7.ResumeLayout(false);
+            this.tableLayoutPanel7.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel9;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel10;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel8;
+        private System.Windows.Forms.Button buttonRead;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel5;
+        private System.Windows.Forms.TextBox textBoxExtraInfo;
+        private System.Windows.Forms.Label labelExtraInfo;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel11;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel12;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel4;
+        private System.Windows.Forms.ComboBox comboBoxWhatDo;
+        private System.Windows.Forms.Label labelWhatDo;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.ComboBox comboBoxWho;
+        private System.Windows.Forms.Label labelWho;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel13;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.TextBox textBoxHowLong;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel6;
+        private System.Windows.Forms.TextBox textBoxStart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel7;
+        private System.Windows.Forms.TextBox textBoxStop;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonDate;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Button buttonClear2;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.ListBox listBoxLogs;
+    }
+}
+
---
+++ Bociek/Form1.cs
@@ -0,0 +1,268 @@
+﻿namespace Bociek
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Data;
+    using System.Drawing;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Text.RegularExpressions;
+    using System.Threading.Tasks;
+    using System.Windows.Forms;
+    using System.Globalization;
+    using ExtensionMethods;
+    public partial class Form1 : Form
+    {
+        String Logs;
+        String Log;
+        static String DateFormat = "dd.MM.yyyy HH:mm:ss";
+        String Today;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            LogsInit();
+            Log = null;
+
+            Today = DateTime.Today.ToString();
+            textBoxStart.Text = Today;
+            textBoxStop.Text = DateFormat;
+
+            textBoxHowLong.Text = "0:0:0";
+        }
+
+        private void LogsInit()
+        {
+            Logs = "Kto? | Co robi? | Od kiedy? | Do kiedy? | Jak długo? | Dodatkowe informacje";
+            listBoxLogs.Items.Add(Logs);
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            if (textBoxStop.Text == DateFormat)
+                textBoxStop.Text = null;
+
+            if (AllIsOk())
+            {
+                // Log = "Kto? | Co robi? | Od kiedy? | Do kiedy? | Jak długo? | Dodatkowe informacje"
+                Log = comboBoxWho.SelectedItem.ToString() + " | " 
+                    + comboBoxWhatDo.SelectedItem.ToString() + " | "
+                    + textBoxStart.Text + " | ";
+
+                if (textBoxStop.Text.Length > 10)//tekst krótszy od 10 oznacza ze nie podano prawidlowego
+                {
+                        Log += textBoxStop.Text + " | " + HowLong() + " | ";
+                        textBoxStart.Text = textBoxStop.Text;
+                }
+                else
+                    if (textBoxHowLong.Text.Length < 5)
+                    {
+                        Log = Log + " | " + " | ";
+                    }
+                    else
+                    {
+                        String temp = DateOfEnd();
+                        Log = Log + temp + " | " + textBoxHowLong.Text + " | ";
+                        textBoxStart.Text = temp;
+                    }
+
+                Log = Log + textBoxExtraInfo.Text;
+                listBoxLogs.Items.Add(Log);
+
+                textBoxStop.Text = null;
+
+                SaveLog(Log);
+
+                textBoxHowLong.Text = type[type.Length - 1];
+
+                listBoxLogs.TopIndex = listBoxLogs.Items.Count - 1;
+
+            }
+            else
+            {
+                MessageBox.Show("Podaj wymagane informacje");
+            }
+        }
+
+        String[] type;
+        private String DateOfEnd()
+        {
+            var mydateStart = textBoxStart.Text.ToString();
+            var dateStart = mydateStart.ToDateTime(DateFormat);
+
+            TimeSpan howLong;
+
+           type = textBoxHowLong.Text.Split(' ');
+            if (type.Length == 1)
+            {
+                String[] howLongTab = textBoxHowLong.Text.Split(':');
+                howLong = new TimeSpan(
+                    Int32.Parse(howLongTab[0]), //godziny
+                    Int32.Parse(howLongTab[1]), //minuty
+                    Int32.Parse(howLongTab[2]) //sekundy
+                    );
+            }
+            else
+            {
+                String[] Start = type[0].Split(':');
+                TimeSpan howLongStart = new TimeSpan(
+                    Int32.Parse(Start[0]), //godziny
+                    Int32.Parse(Start[1]), //minuty
+                    Int32.Parse(Start[2]) //sekundy
+                    );
+
+                
+                String[] Stop = type[1].Split(':');
+
+                TimeSpan howLongStop = new TimeSpan(
+                    Int32.Parse(Stop[0]), //godziny
+                    Int32.Parse(Stop[1]), //minuty
+                    Int32.Parse(Stop[2]) //sekundy
+                    );
+
+                howLong = (howLongStop - howLongStart);
+                textBoxHowLong.Text = howLong.Hours.ToString() + ":" + howLong.Minutes.ToString() + ":" + howLong.Seconds.ToString();
+            }
+
+            var dateEnd = mydateStart.ToDateTime(DateFormat) + howLong;
+            return dateEnd.ToString();
+        }
+
+        private bool AllIsOk()
+        {
+            if (comboBoxWhatDo.SelectedItem == null)
+                return false;
+
+            if (comboBoxWho.SelectedItem == null)
+                return false;
+
+            if (textBoxStart.Text.Length < 10)
+                return false;
+
+            if (textBoxStop.Text == Today)
+                return false;
+
+            return true;
+        }
+
+        private String HowLong()
+        {
+            var mydateStart = textBoxStart.Text.ToString();
+            var dateStart = mydateStart.ToDateTime(DateFormat);
+
+            var mydateStop = textBoxStop.Text.ToString();
+            var dateStop = mydateStop.ToDateTime(DateFormat);
+
+            TimeSpan howLong = dateStop - dateStart;
+
+            return howLong.Hours.ToString() + ":" + howLong.Minutes.ToString() + ":" + howLong.Seconds.ToString();
+        }
+
+        private void SaveLog(String Log)
+        {
+            StreamWriter SW;
+            SW = File.AppendText("Informacje o bocianach - program.txt");
+            SW.WriteLine(Log);
+            SW.Close();
+        }
+
+        private void buttonRead_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog theDialog = new OpenFileDialog();
+            theDialog.Title = "Open Text File";
+            theDialog.Filter = "TXT files|*.txt";
+            if (theDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filename = theDialog.FileName;
+
+                string[] filelines = File.ReadAllLines(filename);
+
+                listBoxLogs.Items.Clear();
+                LogsInit();
+
+                foreach (String elem in filelines)
+                {
+                    Log = Environment.NewLine + elem;
+                    listBoxLogs.Items.Add(Log);
+                }
+
+                listBoxLogs.TopIndex = listBoxLogs.Items.Count - 1;
+            }
+        }
+
+        private void buttonDate_Click(object sender, EventArgs e)
+        {
+            textBoxStop.Text = textBoxStart.Text;
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            textBoxStop.Text = null;
+        }
+
+        private void buttonClear2_Click(object sender, EventArgs e)
+        {
+            textBoxHowLong.Text = null;
+        }
+
+        private void listBoxLogs_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                StringBuilder sb = new StringBuilder();
+                StringBuilder sb1 = new StringBuilder();
+                foreach (object row in listBoxLogs.SelectedItems)
+                {
+                    String[] rowTab = row.ToString().Split('|');
+
+                    for (int i = 1; i < rowTab[3].ToString().Length-1; i++)
+                    {
+                        sb1.Append(rowTab[3].ToString()[i]);
+                    }
+
+                    sb.Append(sb1);
+                    sb.AppendLine();
+                }
+                sb.Remove(sb.Length - 1, 1); // Just to avoid copying last empty row
+                Clipboard.SetData(System.Windows.Forms.DataFormats.Text, sb.ToString());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
+
+namespace ExtensionMethods
+{
+    using System;
+    using System.Globalization;
+
+    public static class DateTimeExtensions
+    {
+        public static DateTime ToDateTime(this string s,
+                  string format = "ddMMyyyy", string cultureString = "en-US")
+        {
+            try
+            {
+                var r = DateTime.ParseExact(
+                    s: s,
+                    format: format,
+                    provider: CultureInfo.GetCultureInfo(cultureString));
+                return r;
+            }
+            catch (FormatException)
+            {
+                throw;
+            }
+            catch (CultureNotFoundException)
+            {
+                throw; // Given Culture is not supported culture
+            }
+        }
+    }
+}
